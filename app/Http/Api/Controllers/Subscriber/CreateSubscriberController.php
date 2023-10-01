bc39d3812f7aab5fd7d2a2a5104ad3d6d053f24d
janramirez <jramirez.ust@gmail.com>
2023-10-01 23:02:13 +0800

    Rename subscriber controller to CreateSubscriberController

--- app/Http/Api/Controllers/Subscriber/CreateSubscriberController.php
+++ app/Http/Api/Controllers/Subscriber/CreateSubscriberController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
-class SubscriberController
+class CreateSubscriberController
 {
     public function store(
         SubscriberData $data,
