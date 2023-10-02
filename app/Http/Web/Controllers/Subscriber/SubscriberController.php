0346aaadadd6a852661224227599043db42ea857
janramirez <jramirez.ust@gmail.com>
2023-10-02 18:37:49 +0800

    Add index method for subscribers list

--- app/Http/Web/Controllers/Subscriber/SubscriberController.php
+++ app/Http/Web/Controllers/Subscriber/SubscriberController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Web\Controllers\Subscriber;
 
+use Domain\Subscriber\Actions\UpsertSubscriberAction;
+use Domain\Subscriber\DataTransferObjects\SubscriberData;
 use Domain\Subscriber\Models\Subscriber;
 use Domain\Subscriber\ViewModels\UpsertSubscriberViewModel;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class SubscriberController
 {
+    public function index(Request $request): Response
+    {
+        return Inertia::render('Subscriber/List', [
+            'model' => new GetSubscribersViewModel($request->get('page',1)),
+        ]);
+    }
+    
     public function create(): Response
     {
         return Inertia::render('Subscriber/Form', [
@@ -21,5 +33,14 @@ class SubscriberController
         return Inertia::render('Subscriber/Form', [
             'model' => new UpsertSubscriberViewModel($subscriber),
         ]);
+    }
+
+    public function store(
+        SubscriberData $data,
+        Request $request
+    ): RedirectResponse {
+        UpsertSubscriberAction::execute($data, $request->user());
+
+        return Redirect::route('subscribers.index');
     }
 }
