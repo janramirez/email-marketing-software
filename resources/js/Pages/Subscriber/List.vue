79777ea6906977f62210e9e253ee608dfca117dd
janramirez <jramirez.ust@gmail.com>
2023-10-02 19:28:45 +0800

    Create frontend vue component to show list of subscribers

--- resources/js/Pages/Subscriber/List.vue
+++ resources/js/Pages/Subscriber/List.vue
@@ -1,7 +1,7 @@
 <script>
-import AuthenticatedLayout from '@/Layouts/Authenticated.vue'
-import { Head, Link } from '@inertiajs/vue3';
-import Pagination from '@/Components/Pagination';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, Link } from "@inertiajs/vue3";
+import Pagination from "@/Components/Pagination.vue";
 
 export default {
     components: {
@@ -32,15 +32,36 @@ export default {
 
             this.$inertia.get(this.model.subscribers.prev_page_url);
         },
-    }
-}
-    
+    },
+};
 </script>
 <template>
-    <Pagination
-        :total="model.total"
-        :current_page="model.subscribers.current_page"
-        @paginated-prev="prevPage()"
-        @paginated-next="nextPage()"
-    ></Pagination>
+    <Head title="Subscribers List" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Subscribers
+            </h2>
+            <Link
+                href="/subscribers/create"
+                as="button"
+                type="button"
+                class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-2"
+            >
+                Add Subscriber
+            </Link>
+            <button
+                class="bg-transparent hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded ml-2"
+                @click="importSubscribers()"
+            >
+                Import Subscribers
+            </button>
+        </template>
+        <Pagination
+            :total="model.total"
+            :current_page="model.subscribers.current_page"
+            @paginated-prev="prevPage()"
+            @paginated-next="nextPage()"
+        ></Pagination>
+    </AuthenticatedLayout>
 </template>
