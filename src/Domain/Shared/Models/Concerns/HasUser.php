43400e5dfcbfc5021de75672dfd4088ce68ee0c2
janramirez <jramirez.ust@gmail.com>
2023-10-02 13:56:07 +0800

    Create common trait HasUser for models with relationship to the user model

---
+++ src/Domain/Shared/Models/Concerns/HasUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Domain\Shared\Models\Concerns;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait HasUser
+{
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new UserScope()); //TODO Shared\Models\Scopes\UserScope
+    }
+}
