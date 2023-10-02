f727adc1e8b80c66f00fa7dd2ae8da684010228b
janramirez <jramirez.ust@gmail.com>
2023-10-02 20:52:35 +0800

    Create console command to import subscribers from csv file

---
+++ app/Console/Commands/Subscriber/SubscriberImportSubscribersCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands\Subscriber;
+
+use Domain\Shared\Models\User;
+use Illuminate\Console\Command;
+
+class SubscriberImportSubscribersCommand extends Command
+{
+    protected $signature = 'subscriber:import {user? : The ID of the user}';
+    protected $description = 'Import subscribers from csv';
+
+    public function handle()
+    {
+        $userId = $this->argument('user') ?? $this->ask('User ID');
+
+        ImportSubscribersJob::dispatch(
+            storage_path('subscribers/subscribers.csv'),
+            User::findOrFail($userId),
+        );
+
+        $this->info("Subscribers are being imported...");
+
+        return self::SUCCESS;
+    }
+}
