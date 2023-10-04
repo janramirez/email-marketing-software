97d268174f73eaf0ba764e9fd284a94db590adc0
janramirez <jramirez.ust@gmail.com>
2023-10-04 22:43:04 +0800

    Create action to send email blasts

---
+++ src/Domain/Mail/Actions/Blast/SendBlastAction.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Domain\Mail\Actions\Blast;
+
+use Domain\Mail\Exceptions\CannotSendBlast;
+use Domain\Mail\Mails\EchoMail;
+use Domain\Mail\Models\Blast\Blast;
+use Domain\Subscriber\Actions\FilterSubscribersAction;
+use Domain\Subscriber\Models\Subscriber;
+use Illuminate\Support\Facades\Mail;
+
+class SendBlastAction
+{
+    public static function execute(Blast $blast): int
+    {
+        /**
+         * check if email blast already sent,
+         * throw an exception if so
+         */
+        if (!$blast->status->canSend()) {
+            throw CannotSendBlast::because(
+                "Email blast already sent at {$blast->sent_at}"
+            );
+        }
+
+        /**
+         * filter subscribers based on blast filters,
+         * then send email
+         */
+        $subscribers = FilterSubscribersAction::execute($blast)
+            ->each(fn (Subscriber $subscriber) =>
+                Mail::to($subscriber)->queue(new EchoMail($blast))
+            );
+        
+        $blast->markAsSent();
+
+        return $subscribers->each(fn (Subscriber $subscriber) =>
+            $blast->sent_mails()->create([
+                'subscriber_id' => $subscriber->id,
+                'user_id' => $blast->user->id,
+            ])
+        )->count();
+    }
+}
