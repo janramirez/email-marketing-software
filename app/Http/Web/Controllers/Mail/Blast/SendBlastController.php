87448a617ab3e700cdedd550d65c9332d9ceff9d
janramirez <jramirez.ust@gmail.com>
2023-10-05 16:36:16 +0800

    Create controller to dispatch send email blast job

---
+++ app/Http/Web/Controllers/Mail/Blast/SendBlastController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Web\Controllers\Mail\Blast;
+
+use Domain\Mail\Jobs\Blast\SendBlastJob;
+use Domain\Mail\Models\Blast\Blast;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class SendBlastController
+{
+    public function __invoke(Blast $blast): Response
+    {
+        SendBlastJob::dispatch($blast);
+
+        return response('', Response::HTTP_ACCEPTED);
+    }
+}
