0a8abf5c6cc92a0da6193850f2423a5b4b5eef14
janramirez <jramirez.ust@gmail.com>
2023-10-05 16:33:48 +0800

    Create job to handle sending email blasts

---
+++ src/Domain/Mail/Jobs/Blast/SendBlastJob.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Domain\Mail\Jobs\Blast;
+
+use Domain\Mail\Actions\Blast\SendBlastAction;
+use Domain\Mail\Models\Blast\Blast;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendBlastJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(private readonly Blast $blast)
+    {
+    }
+
+    public function handle(): void
+    {
+        SendBlastAction::execute($this->blast);
+    }
+}
