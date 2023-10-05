3b2a277c85ae8cfc5c1c85a327974f436378ac0a
janramirez <jramirez.ust@gmail.com>
2023-10-05 15:52:17 +0800

    Clean up controller

--- app/Http/Web/Controllers/Mail/Blast/BlastController.php
+++ app/Http/Web/Controllers/Mail/Blast/BlastController.php
@@ -7,23 +7,21 @@ use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Domain\Mail\DataTransferObjects\Blast\BlastData;
+use Domain\Mail\Models\Blast\Blast;
 use Domain\Mail\ViewModels\Blast\UpsertBlastViewModel;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Response;
 
 class BlastController
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index(): Response
     {
-        return Inertia::render('Blast/List');
+        return Inertia::render('Blast/List', [
+            'model' => Blast::all(),
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create(): Response
     {
         return Inertia::render('Blast/Form',[
@@ -31,9 +29,6 @@ class BlastController
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(
         BlastData $data,
         Request $request
@@ -43,25 +38,16 @@ class BlastController
             return Redirect::route('blasts.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(
         BlastData $data,
         Request $request
@@ -71,9 +57,6 @@ class BlastController
         return Redirect::route('blasts.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
