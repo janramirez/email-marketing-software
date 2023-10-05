88f5c385e7cfa32b18bd83eb1c3ce7b159600f73
janramirez <jramirez.ust@gmail.com>
2023-10-05 14:23:57 +0800

    Add canSend method to determine status of email

--- src/Domain/Mail/Enums/Blast/BlastStatus.php
+++ src/Domain/Mail/Enums/Blast/BlastStatus.php
@@ -6,4 +6,12 @@ enum BlastStatus: string
 {
     case Draft = 'draft';
     case Sent = 'sent';
+
+    public function canSend(): bool
+    {
+        return match ($this) {
+            self::Draft => true,
+            self::Sent => false,
+        };
+    }
 }
