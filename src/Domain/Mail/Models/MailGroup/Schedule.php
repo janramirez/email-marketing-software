d1d195ba7f237f920291897b3750b7dd00692a7e
janramirez <jramirez.ust@gmail.com>
2023-10-09 19:19:59 +0800

    Create model for schedule

---
+++ src/Domain/Mail/Models/MailGroup/Schedule.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Domain\Mail\Models\MailGroup;
+
+use Domain\Shared\Models\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Schedule extends BaseModel
+{
+    protected $fillable = [
+        'delay',
+        'unit',
+        'allowed_days',
+        'scheduled_mail_id',
+    ];
+
+    protected $casts = [
+        'allowed_days' => ScheduleAllowedDaysCast::class, // TODO
+        'unit' => ScheduledMailUnit::class, // TODO
+    ];
+
+    public function scheduled_mail(): BelongsTo
+    {
+        return $this->belongsTo(ScheduledMail::class);
+    }
+
+    // TODO delayInHours()
+}
