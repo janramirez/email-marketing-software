70f461c8973fdf6733af59ef3b94f57125f4e068
janramirez <jramirez.ust@gmail.com>
2023-10-09 21:11:47 +0800

    Refactor to consider querying mail group subscribers

--- src/Domain/Subscriber/Actions/FilterSubscribersAction.php
+++ src/Domain/Subscriber/Actions/FilterSubscribersAction.php
@@ -4,6 +4,7 @@ namespace Domain\Subscriber\Actions;
 
 use Domain\Mail\Contracts\Sendable;
 use Domain\Mail\Models\Blast\Blast;
+use Domain\Mail\Models\MailGroup\ScheduledMail;
 use Domain\Subscriber\Enums\Filters;
 use Domain\Subscriber\Models\Subscriber;
 use Illuminate\Pipeline\Pipeline;
@@ -16,8 +17,18 @@ class FilterSubscribersAction
      */
     public static function execute(Sendable $mail): Collection
     {
+        $subscribers = Subscriber::query();
+        
+        if ($mail instanceof ScheduledMail) {
+            $subscribers = Subscriber::query()
+                ->whereIn(
+                    'id',
+                    $mail->mail_group->subscribers()->select('subscribers.id')->pluck('id')
+                );
+        }
+
         return app(Pipeline::class)
-            ->send(Subscriber::query())
+            ->send($subscribers)
             ->through(self::filters($mail))
             ->thenReturn()
             ->get();
