8fc7bb6d3afec7a72342a4535efe60957b3955db
janramirez <jramirez.ust@gmail.com>
2023-10-09 21:06:02 +0800

    Schedule mail group command to proceed every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('mailgroup:proceed')
+            ->everyMinute()
+            ->appendOutputTo(storage_path('logs/scheduler.log'));
     }
 
     /**
