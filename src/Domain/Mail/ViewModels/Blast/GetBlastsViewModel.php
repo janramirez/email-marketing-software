ab3d88b914859fe0a4715f529e65c2a8204bd286
janramirez <jramirez.ust@gmail.com>
2023-10-11 16:19:01 +0800

    Refactor to use the model's performance() property instead of an action

--- src/Domain/Mail/ViewModels/Blast/GetBlastsViewModel.php
+++ src/Domain/Mail/ViewModels/Blast/GetBlastsViewModel.php
@@ -24,7 +24,7 @@ class GetBlastsViewModel extends ViewModel
          */
         return Blast::all()
             ->mapWithKeys(fn (Blast $blast) => [
-                $blast->id => GetPerformanceAction::execute($blast),
+                $blast->id => $blast->performance(),
             ]);
     }
 }
