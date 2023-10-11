81e6cf6d97952f76a5f2c940f925bacaaa777881
janramirez <jramirez.ust@gmail.com>
2023-10-11 17:02:55 +0800

    Implement query builder method to hydrate new subscribers count data and add necessary methods

--- src/Domain/Shared/ViewModels/GetDashboardViewModel.php
+++ src/Domain/Shared/ViewModels/GetDashboardViewModel.php
@@ -2,17 +2,78 @@
 
 namespace Domain\Shared\ViewModels;
 
+use Domain\Mail\DataTransferObjects\PerformanceData;
+use Domain\Mail\Models\SentMail;
+use Domain\Shared\Filters\DateFilter;
+use Domain\Shared\ValueObjects\Percent;
+use Domain\Subscriber\DataTransferObjects\DailySubscribersData;
 use Domain\Subscriber\DataTransferObjects\NewSubscribersCountData;
+use Domain\Subscriber\Models\Subscriber;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
 
 class GetDashboardViewModel extends ViewModel
 {
     public function newSubscribersCount(): NewSubscribersCountData
     {
         return new NewSubscribersCountData(
-            total: 0,
-            this_month: 0,
-            this_week: 0,
-            today: 0,
+            total: Subscriber::count(),
+            this_month: Subscriber::whereSubscribedBetween(DateFilter::thisMonth())->count(),
+            this_week: Subscriber::whereSubscribedBetween(DateFilter::thisWeek())->count(),
+            today: Subscriber::whereSubscribedBetween(DateFilter::toady())->count(),
         );
+    }
+
+    public function performance(): PerformanceData
+    {
+        $total = SentMail::count();
+
+        return new PerformanceData(
+            total: $total,
+            open_rate: $this->averageOpenRate($total),
+            click_rate: $this->averageClickRate($total),
+        );
+    }
+
+    private function averageOpenRate(int $total): Percent
+    {
+        return Percent::from(
+            SentMail::whereOpened()->count(),
+            $total
+        );
+    }
+
+    private function averageClickRate(int $total): Percent
+    {
+        return Percent::from(
+            SentMail::wherClicked()->count(),
+            $total
+        );
+    }
+
+    /**
+     * @return Collection<DailySubscribersData>
+     */
+    public function dailySubscribers(): Collection
+    {
+        return DB::table('subscribers')
+            ->select(
+                DB::raw("count(*) count, date_format(subscribed_at, '%Y-%m-%d') day")
+            )
+            ->groupBy('day')
+            ->orderByDesc('day')
+            ->whereUserId($this->user->id)
+            ->get()
+            ->map(fn (object $data) => DailySubscribersData::from((array) $data));
+    }
+
+    public function recentSubscribers(): Collection
+    {
+        return Subscriber::with(['form', 'tags'])
+            ->orderByDesc('subscribed_at')
+            ->take(10)
+            ->get()
+            ->map
+            ->getData();
     }
 }
